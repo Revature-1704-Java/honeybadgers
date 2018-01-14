69f3bdd6a2db653230267ccc0efce3d8091f8559
Tzu Chao <chao.tzu5@gmail.com>
2018-01-14 15:20:55 -0500

    more class to interfaces

--- src/main/ngapp/src/app/services/quiz-form.service.ts
+++ src/main/ngapp/src/app/services/quiz-form.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import {QuizAnswer} from '../class/quiz-answer';
+import {QuizAnswer} from '../interfaces/quiz-answer';
 @Injectable()
 export class QuizFormService {
   userAnswer: QuizAnswer[] = [];
