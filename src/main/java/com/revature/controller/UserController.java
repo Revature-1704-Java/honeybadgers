39a9577cbcab17b2edc0d16da641ec658fc600cf
dgcocano <dgcocano@gmail.com>
2018-01-15 21:00:01 +0000

    Update password

--- src/main/java/com/revature/controller/UserController.java
+++ src/main/java/com/revature/controller/UserController.java
@@ -6,9 +6,11 @@ import com.revature.beans.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -33,4 +35,19 @@ public class UserController {
     HttpHeaders responseHeaders = new HttpHeaders();
     return new ResponseEntity("User created", responseHeaders, HttpStatus.ACCEPTED);
   }
+
+  @RequestMapping(value="/{username}", method=RequestMethod.PUT)
+  public ResponseEntity putUser(@RequestBody Users input, @PathVariable String username) {
+    Users user = input;
+    UsersDao usersDao = new UsersDao();
+    Users currentUser = usersDao.getUser(username);
+    HttpHeaders responseHeaders = new HttpHeaders();
+    if(currentUser == null) {
+      return new ResponseEntity("Resource not found", responseHeaders, HttpStatus.NOT_FOUND);
+    }
+    currentUser.setPassword(input.getPassword());    
+    usersDao.updateUser(currentUser);
+    return new ResponseEntity(currentUser, responseHeaders, HttpStatus.OK);
+  }
+
 }
--- src/main/java/com/revature/dao/UsersDao.java
+++ src/main/java/com/revature/dao/UsersDao.java
@@ -65,10 +65,10 @@ public class UsersDao {
 	}
 	
     // update password
-    public void updateUser(Users updatetUser){
+    public void updateUser(Users updatedUser){
 	Session session = HibernateUtil.getSessionFactory().openSession();
 	Transaction tx = session.beginTransaction();
-	session.update(updateUser);
+	session.update(updatedUser);
 	tx.commit();
 	session.close();
     }
