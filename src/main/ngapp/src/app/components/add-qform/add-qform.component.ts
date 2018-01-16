6d658b3b902d08d7e18856523b038f262c3a8629
Tzu Chao <chao.tzu5@gmail.com>
2018-01-16 09:58:19 -0500

    form backbone

--- src/main/ngapp/src/app/components/add-qform/add-qform.component.ts
+++ src/main/ngapp/src/app/components/add-qform/add-qform.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { Validators } from '@angular/forms/src/validators';
 
 @Component({
   selector: 'app-add-qform',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-qform.component.css']
 })
 export class AddQformComponent implements OnInit {
-
-  constructor() { }
+  QForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.QForm = this.fb.group(
+      {
+        Question: ['', Validators.required],
+        Answers: this.fb.array([])
+      }
+    );
+    this.addAnswers();
+    this.addAnswers();
   }
 
+  get Answers(): FormArray {
+    return this.QForm.get('Answers') as FormArray;
+  }
+
+  addAnswers(): void {
+    this.Answers.push(this.fb.group({
+      text: ['', Validators.required],
+      correct: ['', Validators.required]
+    }));
+  }
 }
