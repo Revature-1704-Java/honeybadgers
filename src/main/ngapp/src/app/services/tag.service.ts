32111a54a1b37f42f181b2bfbe9639d69d0f359f
Kei Peralta <cnmperalta@gmail.com>
2018-01-17 11:22:01 -0500

    Make get request to live server

--- src/main/ngapp/src/app/services/tag.service.ts
+++ src/main/ngapp/src/app/services/tag.service.ts
@@ -10,7 +10,8 @@ export class TagService {
   constructor(private http: HttpClient) { }
 
   getTags(): Observable<Tag[]> {
-    this.tags = this.http.get<Tag[]>('../assets/mocktags.json');
+    // this.tags = this.http.get<Tag[]>('../assets/mocktags.json');
+    this.tags = this.http.get<Tag[]>('http://52.14.182.231:8181/tag');
     return this.tags;
   }
 
